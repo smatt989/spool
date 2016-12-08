9e328939175d0c392dd94b486920f35e8ec7c7bd
Matthew Slotkin <matt@Matthews-MacBook-Pro.local>
2016-12-07 18:52:32 -0800

    fixing typo

--- src/main/scala/JettyLauncher.scala
+++ src/main/scala/JettyLauncher.scala
@@ -1,5 +1,4 @@
 import org.eclipse.jetty.server.Server
-import org.eclipse.jetty.servlet.{ DefaultServlet, ServletContextHandler }
 import org.eclipse.jetty.webapp.WebAppContext
 import org.scalatra.servlet.ScalatraListener
 
